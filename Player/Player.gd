4b618cdcaeaba7783ef6a32358e2e68e8112c9b1
Uzuunov-Stefan <uzunov.stefan777@gmail.com>
2020-07-10 18:15:33 +0200

    Added Roll State to movement

--- Player/Player.gd
+++ Player/Player.gd
@@ -2,6 +2,7 @@ extends KinematicBody2D
 
 var velocity = Vector2.ZERO
 var state = MOVE
+var roll_vector = Vector2.LEFT
 
 enum {
 	MOVE,
@@ -16,6 +17,7 @@ onready var animationState = animationTree.get("parameters/playback")
 const MAX_SPEED = 80
 const ACCELERATION = 500
 const FRICTION = 500
+const ROLL_SPEED = 125
 
 func _ready():
 	animationTree.active = true
@@ -25,7 +27,7 @@ func _physics_process(delta):
 		MOVE:
 			move_state(delta)
 		ROLL:
-			pass
+			roll_state(delta)
 		ATTACK:
 			attack_state(delta)
 
@@ -36,24 +38,41 @@ func move_state(delta):
 	input_vector = input_vector.normalized()
 	
 	if input_vector != Vector2.ZERO:
+		roll_vector = input_vector
 		animationTree.set("parameters/Idle/blend_position", input_vector)
 		animationTree.set("parameters/Run/blend_position", input_vector)
 		animationTree.set("parameters/Attack/blend_position", input_vector)
+		animationTree.set("parameters/Roll/blend_position", input_vector)
 		animationState.travel("Run")
 		velocity = velocity.move_toward(input_vector * MAX_SPEED, ACCELERATION * delta)
 	else:
 		animationState.travel("Idle")
 		velocity = velocity.move_toward(Vector2.ZERO, FRICTION * delta)
 	
-	velocity = move_and_slide(velocity)
+	move()
+	
+	if Input.is_action_just_pressed("roll"):
+		state = ROLL
 	
 	if Input.is_action_just_pressed("attack"):
 		state = ATTACK
+
+func roll_state(delta):
+	velocity = roll_vector * ROLL_SPEED
+	animationState.travel("Roll")
+	move()
 
 # warning-ignore:unused_argument
 func attack_state(delta):
 	velocity = Vector2.ZERO
 	animationState.travel("Attack")
+	
+func move():
+	velocity = move_and_slide(velocity)
+	
+func roll_animation_finished():
+	velocity = velocity * 0.8
+	state = MOVE
 
 func attack_animation_finished():
 	state = MOVE
